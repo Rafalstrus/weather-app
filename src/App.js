7a9548ad6425a1b6e9e56966db3098902d971026
Rafalstrus <rafal.strus@onet.eu>
2021-06-24 00:08:03 +0200

    add another things from api call

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React, {useState} from 'react';
+import "./App.css"
  function App() {
   const [city,cityChange] = useState("")
   const [weatherTable, weatherTableChange] = useState([])
    return (
-      <div>
+      <div id="App">
         <input 
         placeholder="City Name"
         onChange={(e)=>{cityChange(e.target.value)}}
         ></input>
-        <button onClick={() => {handleWeatherTable(weatherTableChange,city)}}></button>   
-        {(weatherTable.cod===200)? 
-        <p>{convertFromKelvinToCelsius(weatherTable.main.temp)}</p>: ""}
+        <button  
+        onClick={() => {handleWeatherTable(weatherTableChange,city)} 
+        }
+        id="checkCityButton">Check Weather</button>   
+        {(weatherTable.cod===200)? returnData(weatherTable)
+        : ""}
       </div>
   )
 }
@@ -22,5 +26,23 @@ async function handleWeatherTable(weatherTableChange,city){
 function convertFromKelvinToCelsius(degree){
   return degree-273.15;
 }
+
+function returnData(weatherTable){
+  return (<div>
+  <p>{convertFromKelvinToCelsius(weatherTable.main.temp).toFixed(1)}</p>
+  <p>{convertFromKelvinToCelsius(weatherTable.main.feels_like).toFixed(1)}</p>
+  <p>{convertFromKelvinToCelsius(weatherTable.main.temp_min).toFixed(1)}</p>
+  <p>{convertFromKelvinToCelsius(weatherTable.main.temp_max).toFixed(1)}</p>
+  <p>{(weatherTable.main.pressure).toFixed(1)}</p>
+  <p>{(weatherTable.main.humidity).toFixed(1)}</p>
+  </div>)
+}
+
+document.addEventListener("keyup", function(event) {
+  if (event.keyCode === 13) {
+      event.preventDefault();
+      document.getElementById("checkCityButton").click();
+  }
+});
 
 export default App;
